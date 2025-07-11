1f5bc557f0a3eb5b0e579cc1b0c454996378e86c
Csaba Polyak <csiberius@gmail.com>
2025-07-11 21:53:42 +0200

    fix searchParams types for Next 15

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
 const SignInPage = async ({
   searchParams,
 }: {
-  searchParams?: { redirect?: string };
+  searchParams: Promise<{ redirect?: string }>;
 }) => {
-  const redirectParam = searchParams?.redirect;
+  const { redirect: redirectParam } = await searchParams;
   const session = await getSessionFromCookie();
   const redirectPath = redirectParam ?? REDIRECT_AFTER_SIGN_IN;
 
