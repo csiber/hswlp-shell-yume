c39cbf7e039c541f466abd0f53f76e3ee27967cc
Csaba Polyak <csiberius@gmail.com>
2025-07-18 07:18:46 +0000

    0.0.4.7

--- src/layouts/ShellLayout.tsx
+++ src/layouts/ShellLayout.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import LeftSidebar from "@/components/dashboard/LeftSidebar";
 import TopBar from "@/components/top-bar";
-import RightSidebar from "@/components/dashboard/RightSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 type ShellLayoutProps = {
@@ -15,13 +13,7 @@ export default function ShellLayout({ children }: ShellLayoutProps) {
       <div className="flex h-full w-full flex-col">
         <TopBar />
         <div className="grid flex-1 grid-cols-12">
-          <div className="col-span-2">
-            <LeftSidebar />
-          </div>
           <main className="col-span-8">{children}</main>
-          <div className="hidden lg:block lg:col-span-2">
-            <RightSidebar />
-          </div>
         </div>
       </div>
     </SidebarProvider>
