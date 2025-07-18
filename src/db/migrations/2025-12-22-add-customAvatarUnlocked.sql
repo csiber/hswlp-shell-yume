e8d54ebe3e4a7d415e540ac65c0564f72d360481
Csaba Polyak <csiberius@gmail.com>
2025-07-18 17:51:10 +0000

    0.0.5.8

--- src/db/migrations/2025-12-22-add-customAvatarUnlocked.sql
+++
@@ -1,2 +0,0 @@
--- Add custom_avatar_unlocked column if missing
-ALTER TABLE user ADD COLUMN IF NOT EXISTS custom_avatar_unlocked INTEGER DEFAULT 0;
--- src/db/migrations/2025-12-22-add-profileFrameEnabled.sql
+++
@@ -1,2 +0,0 @@
--- Adds profile_frame_enabled column if it does not exist
-ALTER TABLE user ADD COLUMN IF NOT EXISTS profile_frame_enabled INTEGER DEFAULT 0;
