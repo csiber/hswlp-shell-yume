93a2f1ad3e8b349509749fcf448dbf3d2395e562
Csaba Polyak <csiberius@gmail.com>
2025-07-21 10:52:34 +0200

    feat(register): connect to global db

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,7 @@ import * as schema from "./schema";
 // Egyszerű segédfüggvény a D1 adatbázis eléréséhez Workers környezetben
 
 export let db: DrizzleD1Database<typeof schema> | null = null;
-export let globalDb: DrizzleD1Database<typeof schema> | null = null;
+
 
 export const getDB = async () => {
   if (db) {
@@ -32,19 +32,6 @@ export const getDB = async () => {
 };
 
 export const getGlobalDB = async () => {
-  if (globalDb) {
-    return globalDb;
-  }
-
-  const { env } = await getCloudflareContext({ async: true });
-
-  const database = env.DB_GLOBAL ?? env.DB ?? env.NEXT_TAG_CACHE_D1;
-
-  if (!database) {
-    throw new Error("D1 database not found");
-  }
-
-  globalDb = drizzle(database, { schema, logger: true });
-
-  return globalDb;
-};
+  const { env } = getCloudflareContext()
+  return drizzle(env.DB_GLOBAL)
+}
