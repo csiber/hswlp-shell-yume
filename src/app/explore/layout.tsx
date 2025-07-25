306e462d4c068258206defcbd82e9c07689c90e6
Csaba Polyak <csiberius@gmail.com>
2025-07-25 13:00:54 +0200

    feat: add public layout to explore pages

---
+++ src/app/explore/layout.tsx
@@ -0,0 +1,5 @@
+import NavFooterLayout from '@/layouts/NavFooterLayout'
+
+export default function ExploreLayout({ children }: { children: React.ReactNode }) {
+  return <NavFooterLayout>{children}</NavFooterLayout>
+}
