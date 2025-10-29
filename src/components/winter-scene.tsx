c18fd01797b35f0fd60d1b3b31b56fd9ff99a39d
Csaba Polyak <csiberius@gmail.com>
2025-10-29 13:43:43 +0100

    Téli skin és hóesés animáció

---
+++ src/components/winter-scene.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export function WinterScene() {
+  return (
+    <div aria-hidden className="winter-background">
+      <div className="winter-gradient" />
+      <div className="winter-noise" />
+      <div className="winter-snow winter-snow--far" />
+      <div className="winter-snow winter-snow--mid" />
+      <div className="winter-snow winter-snow--near" />
+    </div>
+  );
+}
